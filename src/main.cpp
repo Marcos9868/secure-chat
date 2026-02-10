1421bc2036ac05f7b29a93020599a98d6609d955
Marcos9868 <marcos_m_ferreira@outlook.com>
2026-02-10 19:18:28 -0300

    Fix: main
    Add basic structure to validate simple message send

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include <sodium.h>
 #include <iostream>
+#include <ctime>
 #include "core/crypto/CryptoInit.hpp"
 #include "core/crypto/KeyPair.hpp"
 #include "core/identity/identity.hpp"
+#include "core/message/Message.hpp"
 
 int main() {
   crypto::CryptoInit::init();
@@ -10,8 +12,15 @@ int main() {
   auto kp = crypto::KeyPair::generate();
   identity::Identity id(kp.publicKey());
 
-  std::cout << "User ID: " << id.userId() << std::endl;
-  std::cout << "Fingerprint: " << id.fingerprint() << std::endl;
-  std::cout << "Secure Chat core initialized";
-  return 0;
+  message::Message msg(
+    id.userId(),
+    "receiver-id",
+    "Hello Secure World",
+    static_cast<std::uint64_t>(std::time(nullptr))
+  );
+
+  std::cout << "From: " << msg.senderId() << "\n";
+  std::cout << "To: " << msg.receiverId() << "\n";
+  std::cout << "Payload: " << msg.payload() << "\n";
+ return 0;
 }
